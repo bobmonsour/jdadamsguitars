6a1c7ef3bdf539c43622a4c5ff1aab5a43775601
Bob Monsour <bob.monsour@gmail.com>
2023-10-01 07:21:19 -0700

    More markup and CSS

---
+++ eleventy.config.js
@@ -0,0 +1,31 @@
+const bundlerPlugin = require("@11ty/eleventy-plugin-bundle");
+const Image = require("@11ty/eleventy-img");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(bundlerPlugin);
+  eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
+    let metadata = await Image(src, {
+      widths: [300, 600, 900, 1200],
+      formats: ["avif", "jpeg", "webp"],
+      urlPath: "/images",
+      outputDir: "_site/images",
+    });
+    let imageAttributes = {
+      alt,
+      sizes,
+      decoding: "async",
+    };
+    // You bet we throw an error on a missing alt (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+
+  // Return your Object options:
+  return {
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+    },
+  };
+};
