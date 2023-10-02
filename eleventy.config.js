012ecde80840836fa4be8b723c27fcbff9987c54
Bob Monsour <bob.monsour@gmail.com>
2023-10-02 13:36:52 -0700

    Added self-hosted fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,6 +2,7 @@ const bundlerPlugin = require("@11ty/eleventy-plugin-bundle");
 const Image = require("@11ty/eleventy-img");
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/fonts");
   eleventyConfig.addPlugin(bundlerPlugin);
   eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
     let metadata = await Image(src, {
